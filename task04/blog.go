13e1036414d2a736213d34483201aa4169b40575
huangxuekun <wolfhuangxuekun@163.com>
2025-11-12 19:40:43 +0800

    task04 blog

--- task04/blog.go
+++ task04/blog.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -34,10 +41,27 @@ type Comment struct {
 	UserID  uint
 }
 
+type UserRegisterRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
 type UserLoginRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+// 新增文章
+type CreatePostRequest struct {
+	Title   string `json:"title" binding:"required"`
+	Content string `json:"content" binding:"required"`
+}
+
+// 更新文章
+type UpdatePostRequest struct {
+	Title   string `json:"title" binding:"required"`
+	Content string `json:"content" binding:"required"`
 }
 
 func Connect() *gorm.DB {
@@ -48,19 +72,131 @@ func Connect() *gorm.DB {
 	return db
 }
 
+var jwtSecret = []byte(os.Getenv("BLOG_SECRET"))
+
 func main() {
 	db := Connect()
 	db.AutoMigrate(&User{}, &Post{}, &Comment{})
 	r := gin.Default()
 
 	//用户注册
-	register(r, db)
+	Register(r, db)
 
 	//用户登录
+	Login(r, db)
 
-	r.GET("/posts", func(c *gin.Context) {
+	group := r.Group("/")
+	group.Use(JwtAuthMiddleware)
+
+	//新增文章
+	group.POST("/post", func(c *gin.Context) {
+		var createPost CreatePostRequest
+		if err := c.ShouldBindBodyWithJSON(&createPost); err != nil {
+			c.JSON(400, gin.H{"message": "参数绑定错误或缺失必传参数", "error": err.Error()})
+			return
+		}
+		userID, _ := c.Get("userID")
+		db.Create(&Post{UserID: userID.(uint), Title: createPost.Title, Content: createPost.Content})
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "创建文章成功",
+		})
+	})
+
+	//删除文章
+	group.DELETE("/post/:id", func(c *gin.Context) {
+		idStr := c.Param("id")
+		if idStr == "" {
+			c.JSON(400, gin.H{"message": "缺少必要的路径参数 id"})
+			c.Abort()
+			return
+		}
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			c.JSON(400, gin.H{"message": "无效的id参数"})
+		}
+
+		userID, _ := c.Get("userID")
+
+		var existsPost Post
+		db.First(&existsPost, id)
+		if existsPost.ID == 0 {
+			c.JSON(500, gin.H{
+				"message": "文章不存在",
+			})
+			return
+		}
+
+		if existsPost.UserID != userID {
+			c.JSON(500, gin.H{
+				"message": "只能删除自己的文章",
+			})
+			return
+		}
+
+		db.Delete(&existsPost)
+		c.JSON(200, gin.H{
+			"message": "删除文章成功",
+		})
+	})
+
+	//更新文章
+	group.PATCH("/post/:id", func(c *gin.Context) {
+		idStr := c.Param("id")
+		if idStr == "" {
+			c.JSON(400, gin.H{"message": "缺少必要的路径参数 id"})
+			c.Abort()
+			return
+		}
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			c.JSON(400, gin.H{"message": "无效的id参数"})
+		}
+
+		userID, _ := c.Get("userID")
+
+		var existsPost Post
+		db.First(&existsPost, id)
+		if existsPost.ID == 0 {
+			c.JSON(500, gin.H{
+				"message": "文章不存在",
+			})
+			return
+		}
+
+		if existsPost.UserID != userID {
+			c.JSON(500, gin.H{
+				"message": "只能更新自己的文章",
+			})
+			return
+		}
+		var updatePost UpdatePostRequest
+		if err := c.ShouldBindBodyWithJSON(&updatePost); err != nil {
+			c.JSON(400, gin.H{"message": "参数绑定错误或缺失必传参数", "error": err.Error()})
+			return
+		}
+
+		existsPost.Title = updatePost.Title
+		existsPost.Content = updatePost.Content
+
+		db.Model(&Post{}).Where("id = ?", id).Updates(&Post{Title: updatePost.Title, Content: updatePost.Content})
+		c.JSON(200, gin.H{
+			"message": "更新文章成功",
+		})
+	})
+
+	//获取所有的文章列表
+	r.GET("/posts", func(c *gin.Context) {
+
+		var posts []Post
+		db.Model(&Post{}).Preload("Comments").Find(&posts)
+		for post := range posts {
+			fmt.Println(post)
+		}
+
+		c.JSON(200, gin.H{
+			"data": posts,
 		})
 	})
 
@@ -68,61 +204,130 @@ func main() {
 }
 
 // 注册
-func register(r *gin.Engine, db *gorm.DB) {
+func Register(r *gin.Engine, db *gorm.DB) {
 	r.POST("/register", func(c *gin.Context) {
-		var userLoginRequest UserLoginRequest
-		err := c.MustBindWith(&userLoginRequest, binding.JSON)
-		if err != nil {
+		var userRegisterRequest UserRegisterRequest
+		if err := c.ShouldBindBodyWithJSON(&userRegisterRequest); err != nil {
+			c.JSON(400, gin.H{"message": "参数绑定错误", "error": err.Error()})
 			return
 		}
 		//判断用户名是否存在
 		var existsUser User
-		db.Model(&User{}).Where("username = ?", userLoginRequest.Username).First(&existsUser)
+		db.Model(&User{}).Where("username = ?", userRegisterRequest.Username).First(&existsUser)
 		if existsUser.ID != 0 {
 			c.JSON(500, gin.H{
-				"message": "username already exists",
+				"message": "用户名称已存在",
 			})
 			return
 		}
 		//密码加密
-		hash, err := bcrypt.GenerateFromPassword([]byte(userLoginRequest.Password), bcrypt.DefaultCost)
+		hash, err := bcrypt.GenerateFromPassword([]byte(userRegisterRequest.Password), bcrypt.DefaultCost)
 		if err != nil {
 			panic(err)
 		}
 
-		db.Create(&User{Username: userLoginRequest.Username, Password: string(hash), Email: userLoginRequest.Email})
+		db.Create(&User{Username: userRegisterRequest.Username, Password: string(hash), Email: userRegisterRequest.Email})
 		c.JSON(200, gin.H{
-			"message": "register success",
+			"message": "注册成功",
 		})
 	})
 }
 
 // 登录
-func login(r *gin.Engine, db *gorm.DB) {
-	r.POST("/register", func(c *gin.Context) {
+func Login(r *gin.Engine, db *gorm.DB) {
+	r.POST("/login", func(c *gin.Context) {
 		var userLoginRequest UserLoginRequest
-		err := c.MustBindWith(&userLoginRequest, binding.JSON)
-		if err != nil {
+		if err := c.ShouldBindBodyWithJSON(&userLoginRequest); err != nil {
+			c.JSON(400, gin.H{"message": "参数绑定错误", "error": err.Error()})
 			return
 		}
 		//判断用户名是否存在
 		var existsUser User
 		db.Model(&User{}).Where("username = ?", userLoginRequest.Username).First(&existsUser)
-		if existsUser.ID != 0 {
+		if existsUser.ID == 0 {
 			c.JSON(500, gin.H{
-				"message": "username already exists",
+				"message": "用户不存在",
 			})
 			return
 		}
-		//密码加密
-		hash, err := bcrypt.GenerateFromPassword([]byte(userLoginRequest.Password), bcrypt.DefaultCost)
+		//密码校验
+		err := bcrypt.CompareHashAndPassword([]byte(existsUser.Password), []byte(userLoginRequest.Password))
 		if err != nil {
-			panic(err)
+			c.JSON(500, gin.H{
+				"message": "用户与密码不匹配",
+			})
+			return
 		}
 
-		db.Create(&User{Username: userLoginRequest.Username, Password: string(hash), Email: userLoginRequest.Email})
+		// 生成jwt token
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"userID":   existsUser.ID,
+			"username": existsUser.Username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+		tokenString, err := token.SignedString(jwtSecret)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"message": "生成jwt token失败",
+			})
+			return
+		}
 		c.JSON(200, gin.H{
-			"message": "register success",
+			"message": "登录成功",
+			"token":   tokenString,
 		})
 	})
+}
+
+func JwtAuthMiddleware(c *gin.Context) {
+	jwtToken := c.GetHeader("Authorization")
+	if jwtToken == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "请求头缺失",
+		})
+		c.Abort()
+		return
+	}
+
+	parts := strings.Split(jwtToken, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "请求头格式错误",
+		})
+		c.Abort()
+		return
+	}
+
+	token, err := jwt.Parse(parts[1], func(token *jwt.Token) (interface{}, error) {
+		// 校验算法是否符合预期
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtSecret, nil
+	})
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "非法的token" + err.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// 校验是否过期
+		if exp, ok := claims["exp"].(float64); ok {
+			if time.Now().Unix() > int64(exp) {
+				c.JSON(http.StatusUnauthorized, gin.H{"message": "token 过期"})
+				c.Abort()
+				return
+			}
+		}
+
+		c.Set("username", claims["username"].(string))
+		c.Set("userID", uint(claims["userID"].(float64)))
+		c.Next()
+		return
+	}
+
 }
--- task04/go.mod
+++ task04/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.11.0
 	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 )
 
 require (
@@ -13,6 +14,7 @@ require (
 	github.com/bytedance/sonic v1.14.2 // indirect
 	github.com/bytedance/sonic/loader v0.4.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gabriel-vasile/mimetype v1.4.11 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
